8d193567cac6a77b7a6612828ef98f858e1c1db6
Junel Solis <junel.solis@abo.fi>
2025-07-23 09:03:12 +0300

    *clarify init docstring

--- src/ome_zarr_writer/image.py
+++ src/ome_zarr_writer/image.py
@@ -50,7 +50,7 @@ class OmeZarrImage:
                 of dimensions of the input image, or a dictionary that specifies units for each
                 dimension. Dictionary format:
                 - Per-dimension units: {"t": "second", "z": "micrometer", "y": "micrometer", "x": "micrometer"}
-            downscale_method: Method to use for downscaling. Gaussian is default and currently the only supported method.
+            downscale_method: Method to use for downscaling. Either Gaussian filter or nearest-neighbor interpolation; default "gaussian". Use Gaussian filtering for intensity images to avoid aliasing artifacts in downscaled images. Label images should be downscaled using nearest-neighbor interpolation.
                 Another option like "nearest-neighbor" is being added.
             scale_transformations: Optional dictionary specifying scale values for dimensions.
                 Examples:

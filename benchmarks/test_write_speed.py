4775c5be6f19999b8ba3b3a41cd4e516059849a3
Junel Solis <junel.solis@abo.fi>
2026-01-05 23:42:06 +0200

    *update benchmark test

--- benchmarks/test_write_speed.py
+++ benchmarks/test_write_speed.py
@@ -6,6 +6,7 @@ import shutil
 from time import time
 # from dask.distributed import Client
 import dask
+import dask.array as da
 import numpy as np
 import zarr
 import zarr.codecs
@@ -21,7 +22,8 @@ if __name__ == '__main__':
 
     # Declare 3D array.
     # Benchmark writing using both zarrs (Rust) and zarr-python backends.
-    array = np.random.randint(0, 65535, size=(300, 2048, 2048), dtype=np.uint16)
+    # array = np.random.randint(0, 65535, size=(300, 2048, 2048), dtype=np.uint16)
+    array = da.random.randint(0, 65535, size=(300, 2048, 2048), dtype=np.uint16, chunks=(10, 128, 128)).compute()
     dims = ["z", "y", "x"]
     axis_units = {"z": "micrometer", "y": "micrometer", "x": "micrometer"}
 
@@ -53,29 +55,29 @@ if __name__ == '__main__':
     print(f"Rust - Elapsed time: {end_time - start_time:.2f} seconds")
 
 
-    image = OmeZarrImage(
-        path="benchmark.ome.zarr",
-        image=array,
-        dims=dims,
-        axis_units=axis_units,
-        scale_transformations=scale_transformations,
-        downscale_levels=4,
-        zarr_backend="zarr-python",
-        overwrite=True,
-    )
+    # image = OmeZarrImage(
+    #     path="benchmark.ome.zarr",
+    #     image=array,
+    #     dims=dims,
+    #     axis_units=axis_units,
+    #     scale_transformations=scale_transformations,
+    #     downscale_levels=4,
+    #     zarr_backend="zarr-python",
+    #     overwrite=True,
+    # )
 
-    start_time = time()
-    image.write(
-        chunks=(10, 128, 128),
-        shards=(30, 1024, 1024),
-        compressors=[
-            zarr.codecs.BloscCodec(
-                cname="zstd", clevel=5, shuffle=zarr.codecs.BloscShuffle.bitshuffle
-            )
-        ],
-    )
+    # start_time = time()
+    # image.write(
+    #     chunks=(10, 128, 128),
+    #     shards=(30, 1024, 1024),
+    #     compressors=[
+    #         zarr.codecs.BloscCodec(
+    #             cname="zstd", clevel=5, shuffle=zarr.codecs.BloscShuffle.bitshuffle
+    #         )
+    #     ],
+    # )
 
-    end_time = time()
-    print(f"Python - Elapsed time: {end_time - start_time:.2f} seconds")
+    # end_time = time()
+    # print(f"Python - Elapsed time: {end_time - start_time:.2f} seconds")
 
-    shutil.rmtree("benchmark.ome.zarr")
+    # shutil.rmtree("benchmark.ome.zarr")
